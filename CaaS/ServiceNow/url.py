51f1075e57fbf9ec3bd64843867bbbb00b5b9989
deepakb-sacumen <58218976+deepakb-sacumen@users.noreply.github.com>
2020-02-11 17:35:03 +0530

    Servicenow view is added. (#13)
    * UI changes are done
    
    * HTML for Rapid7 and Servicenow
    
    * Changes done in connector for adding menu in dashboard
    
    * Servicenow View file is added

--- CaaS/ServiceNow/url.py
+++ CaaS/ServiceNow/url.py
@@ -4,4 +4,6 @@ from .import views
 
 urlpatterns = [
     path('save_servicenow/', views.Save_servicenow.as_views()),
+    path('test_servicenow/', views.test_servicenow, name='servicenow-test'),
+    path('servicenow_config/', views.servicenow_config, name='servicenow-config'),
 ]
